9450ad670506408cf395956d0a475b8f457eb342
wen.MR <794982456@qq.com>
2019-09-17 16:34:32 +0800

    11111

--- Test916/src/lalala.java
+++ Test916/src/lalala.java
@@ -2,7 +2,7 @@ public class lalala {
     public static void main(String[] args) {
         for (int i = 0; i < 200 ; i++) {
 
-            new Thread(){//1111请问请问
+            new Thread(){//1111请问请问1111
                 public void run(){
                     test0916.INSTANCE.getceshi();
                 }
